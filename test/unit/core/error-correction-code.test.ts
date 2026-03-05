3f38fcc273ea73c86925d8bb504b4ceb4583a82e
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-05 23:15:10 +0100

    Port error-correction-code.test.ts

--- test/unit/core/error-correction-code.test.ts
+++ test/unit/core/error-correction-code.test.ts
@@ -1,11 +1,11 @@
+import { test, expect } from 'vitest'
 import * as Utils from '#core/utils.js'
 import * as Version from '#core/version.js'
 import * as ECLevel from '#core/error-correction-level.js'
 import * as ECCode from '#core/error-correction-code.js'
 import * as Mode from '#core/mode.js'
 
-import { test } from 'tap'
-test('Error correction codewords', function (t) {
+test('Error correction codewords', () => {
   const levels = [ECLevel.L, ECLevel.M, ECLevel.Q, ECLevel.H]
 
   for (let v = 1; v <= 40; v++) {
@@ -17,37 +17,33 @@ test('Error correction codewords', function (t) {
 
       const expectedCodewords = totalCodewords - dataCodewords
 
-      t.equal(
+      // TODO: fix type errors for parameters
+      expect(
         ECCode.getTotalCodewordsCount(v, levels[l]),
-        expectedCodewords,
         'Should return correct codewords number',
-      )
+      ).toEqual(expectedCodewords)
     }
   }
 
-  t.equal(
+  expect(
     ECCode.getTotalCodewordsCount(1),
-    undefined,
     'Should return undefined if EC level is not specified',
-  )
-
-  t.end()
+  ).toEqual(undefined)
 })
 
-test('Error correction blocks', function (t) {
+test('Error correction blocks', () => {
   const levels = [ECLevel.L, ECLevel.M, ECLevel.Q, ECLevel.H]
 
   for (let v = 1; v <= 40; v++) {
     for (let l = 0; l < levels.length; l++) {
-      t.ok(ECCode.getBlocksCount(v, levels[l]), 'Should return a positive number')
+      expect(
+        ECCode.getBlocksCount(v, levels[l]),
+        'Should return a positive number',
+      ).toBeGreaterThan(0)
     }
   }
 
-  t.equal(
-    ECCode.getBlocksCount(1),
+  expect(ECCode.getBlocksCount(1), 'Should return undefined if EC level is not specified').toEqual(
     undefined,
-    'Should return undefined if EC level is not specified',
   )
-
-  t.end()
 })
