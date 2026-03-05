9aee89d44e4ebf20519283f65ac17a71e071e808
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-05 22:52:47 +0100

    Port bit-buffer.test.ts

---
+++ test/unit/core/bit-buffer.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from 'vitest'
+import BitBuffer from '#core/bit-buffer.js'
+
+test('Bit Buffer', () => {
+  const testData = 0x41 // 'A'
+  const expectedDataBits = [false, true, false, false, false, false, false, true]
+
+  const bitBuffer = new BitBuffer()
+
+  expect(bitBuffer.getLengthInBits(), 'Initial length should be 0').toEqual(0)
+
+  bitBuffer.put(testData, 8)
+  expect(bitBuffer.getLengthInBits(), 'Length should be 8').toEqual(8)
+
+  for (let i = 0; i < 8; i++) {
+    expect(bitBuffer.get(i), 'Should return correct bit value').toEqual(expectedDataBits[i])
+  }
+})
