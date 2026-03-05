8bb4a4779dbddc98e69cca9b79e015e78827bc60
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-05 23:21:22 +0100

    Port finder-pattern.test.ts

---
+++ test/unit/core/finder-pattern.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from 'vitest'
+import * as pattern from '#core/finder-pattern.js'
+
+test('Finder pattern', () => {
+  for (let i = 1; i <= 40; i++) {
+    expect(pattern.getPositions(i).length, 'Should always return 3 pattern positions').toEqual(3)
+  }
+})
