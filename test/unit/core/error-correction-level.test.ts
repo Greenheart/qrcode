69c55a64527c36dfcf4035c8feebebd466a78778
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-05 23:20:06 +0100

    Port error-correction-level.test.ts

---
+++ test/unit/core/error-correction-level.test.ts
@@ -0,0 +1,46 @@
+import { test, expect } from 'vitest'
+import * as ECLevel from '#core/error-correction-level.js'
+
+const EC_LEVELS = [ECLevel.L, ECLevel.M, ECLevel.Q, ECLevel.H]
+
+test('Error level from input value', () => {
+  const values = [
+    ['l', 'low'],
+    ['m', 'medium'],
+    ['q', 'quartile'],
+    ['h', 'high'],
+  ]
+
+  for (let l = 0; l < values.length; l++) {
+    for (let i = 0; i < values[l].length; i++) {
+      expect(ECLevel.from(values[l][i])).toEqual(EC_LEVELS[l])
+      expect(ECLevel.from(values[l][i].toUpperCase())).toEqual(EC_LEVELS[l])
+    }
+  }
+
+  expect(ECLevel.from(ECLevel.L), 'Should return passed level if value is valid').toEqual(ECLevel.L)
+  expect(
+    ECLevel.from(undefined, ECLevel.M),
+    'Should return default level if value is undefined',
+  ).toEqual(ECLevel.M)
+  expect(ECLevel.from('', ECLevel.Q), 'Should return default level if value is invalid').toEqual(
+    ECLevel.Q,
+  )
+})
+
+test('Error level validity', () => {
+  for (let l = 0; l < EC_LEVELS.length; l++) {
+    expect(ECLevel.isValid(EC_LEVELS[l]), 'Should return true if error level is valid').toEqual(
+      true,
+    )
+  }
+
+  expect(ECLevel.isValid(undefined), 'Should return false if level is undefined').toEqual(false)
+  expect(ECLevel.isValid({}), 'Should return false if bit property is undefined').toEqual(false)
+  expect(ECLevel.isValid({ bit: -1 }), 'Should return false if bit property value is < 0').toEqual(
+    false,
+  )
+  expect(ECLevel.isValid({ bit: 4 }), 'Should return false if bit property value is > 3').toEqual(
+    false,
+  )
+})
