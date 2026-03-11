d793db13069a7a7a0ce0a7c35ba7a4c6d5bd1565
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-11 14:03:53 +0100

    Convert AlphanumericData to a class

--- lib/core/alphanumeric-data.ts
+++ lib/core/alphanumeric-data.ts
@@ -1,3 +1,4 @@
+import type BitBuffer from './bit-buffer.ts'
 import * as Mode from './mode.ts'
 /**
  * Array of characters available in alphanumeric mode
@@ -5,10 +6,8 @@ import * as Mode from './mode.ts'
  * As per QR Code specification, to each character
  * is assigned a value from 0 to 44 which in this case coincides
  * with the array index
- *
- * @type {Array}
  */
-const ALPHA_NUM_CHARS = [
+const ALPHA_NUM_CHARS: readonly string[] = [
   '0',
   '1',
   '2',
@@ -56,44 +55,53 @@ const ALPHA_NUM_CHARS = [
   ':',
 ]
 
-function AlphanumericData(data) {
-  this.mode = Mode.ALPHANUMERIC
-  this.data = data
-}
+// IDEA: Maybe create a Map with the pre-computed indexOf for all Alphanumeric characters?
+// This could give faster lookups during runtime compared to indexOf which always has to re-compute.
+// The Map could be created with new Map(ALPHA_NUM_CHARS.map(c => [c, ALPHA_NUM_CHARS.indexOf(c)]))
+// A slight tiny memory increase, but much faster QR code encoding
+// It would reduce CPU usage though, which would be nice
 
-AlphanumericData.getBitsLength = function getBitsLength(length) {
-  return 11 * Math.floor(length / 2) + 6 * (length % 2)
-}
+export default class AlphanumericData {
+  mode = Mode.ALPHANUMERIC
+  data: string
 
-AlphanumericData.prototype.getLength = function getLength() {
-  return this.data.length
-}
-
-AlphanumericData.prototype.getBitsLength = function getBitsLength() {
-  return AlphanumericData.getBitsLength(this.data.length)
-}
-
-AlphanumericData.prototype.write = function write(bitBuffer) {
-  let i
-
-  // Input data characters are divided into groups of two characters
-  // and encoded as 11-bit binary codes.
-  for (i = 0; i + 2 <= this.data.length; i += 2) {
-    // The character value of the first character is multiplied by 45
-    let value = ALPHA_NUM_CHARS.indexOf(this.data[i]) * 45
-
-    // The character value of the second digit is added to the product
-    value += ALPHA_NUM_CHARS.indexOf(this.data[i + 1])
-
-    // The sum is then stored as 11-bit binary number
-    bitBuffer.put(value, 11)
+  constructor(data: string) {
+    this.data = data
   }
 
-  // If the number of input data characters is not a multiple of two,
-  // the character value of the final character is encoded as a 6-bit binary number.
-  if (this.data.length % 2) {
-    bitBuffer.put(ALPHA_NUM_CHARS.indexOf(this.data[i]), 6)
+  static getBitsLength(length: number) {
+    return 11 * Math.floor(length / 2) + 6 * (length % 2)
+  }
+
+  // TODO: Why have both a static and an instance method?
+  getBitsLength() {
+    return AlphanumericData.getBitsLength(this.data.length)
+  }
+
+  getLength() {
+    return this.data.length
+  }
+
+  write(bitBuffer: BitBuffer) {
+    let i: number
+
+    // Input data characters are divided into groups of two characters
+    // and encoded as 11-bit binary codes.
+    for (i = 0; i + 2 <= this.data.length; i += 2) {
+      // The character value of the first character is multiplied by 45
+      let value = ALPHA_NUM_CHARS.indexOf(this.data[i]) * 45
+
+      // The character value of the second digit is added to the product
+      value += ALPHA_NUM_CHARS.indexOf(this.data[i + 1])
+
+      // The sum is then stored as 11-bit binary number
+      bitBuffer.put(value, 11)
+    }
+
+    // If the number of input data characters is not a multiple of two,
+    // the character value of the final character is encoded as a 6-bit binary number.
+    if (this.data.length % 2) {
+      bitBuffer.put(ALPHA_NUM_CHARS.indexOf(this.data[i]), 6)
+    }
   }
 }
-
-export default AlphanumericData
