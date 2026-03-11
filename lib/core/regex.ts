ddc95c35d322cff10c0ee15d34f2f6a487572c6b
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-11 19:32:07 +0100

    Create regex in one operation and use const

--- lib/core/regex.ts
+++ lib/core/regex.ts
@@ -1,11 +1,10 @@
 const numeric = '[0-9]+'
 const lettersAndCharacters = '[A-Z $%*+\\-./:]+'
-let kanji =
-  '(?:[u3000-u303F]|[u3040-u309F]|[u30A0-u30FF]|' +
-  '[uFF00-uFFEF]|[u4E00-u9FAF]|[u2605-u2606]|[u2190-u2195]|u203B|' +
-  '[u2010u2015u2018u2019u2025u2026u201Cu201Du2225u2260]|' +
-  '[u0391-u0451]|[u00A7u00A8u00B1u00B4u00D7u00F7])+'
-kanji = kanji.replace(/u/g, '\\u')
+
+const kanji = `(?:[u3000-u303F]|[u3040-u309F]|[u30A0-u30FF]|\
+[uFF00-uFFEF]|[u4E00-u9FAF]|[u2605-u2606]|[u2190-u2195]|u203B|\
+[u2010u2015u2018u2019u2025u2026u201Cu201Du2225u2260]|\
+[u0391-u0451]|[u00A7u00A8u00B1u00B4u00D7u00F7])+`.replace(/u/g, '\\u')
 
 const byte = '(?:(?![A-Z0-9 $%*+\\-./:]|' + kanji + ')(?:.|[\r\n]))+'
 
