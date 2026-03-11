fcd4c29ee656d0883cc30b14d96bbf4e3cd46483
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-11 14:55:46 +0100

    Convert ReedSolomonEncoder to a class

--- lib/core/reed-solomon-encoder.ts
+++ lib/core/reed-solomon-encoder.ts
@@ -1,56 +1,59 @@
 import * as Polynomial from './polynomial.ts'
 
-function ReedSolomonEncoder(degree) {
-  this.genPoly = undefined
-  this.degree = degree
+// IDEA: Maybe import simple functions from the polynomial instead of the namespace wildcard import?
 
-  if (this.degree) this.initialize(this.degree)
-}
+export default class ReedSolomonEncoder {
+  genPoly: Uint8Array<ArrayBuffer> | undefined
+  degree: number
 
-/**
- * Initialize the encoder.
- * The input param should correspond to the number of error correction codewords.
- *
- * @param  {Number} degree
- */
-ReedSolomonEncoder.prototype.initialize = function initialize(degree) {
-  // create an irreducible generator polynomial
-  this.degree = degree
-  this.genPoly = Polynomial.generateECPolynomial(this.degree)
-}
+  constructor(degree: number) {
+    this.genPoly = undefined
+    this.degree = degree
 
-/**
- * Encodes a chunk of data
- *
- * @param  {Uint8Array} data Buffer containing input data
- * @return {Uint8Array}      Buffer containing encoded data
- */
-ReedSolomonEncoder.prototype.encode = function encode(data) {
-  if (!this.genPoly) {
-    throw new Error('Encoder not initialized')
+    if (this.degree) this.initialize(this.degree)
   }
 
-  // Calculate EC for this data block
-  // extends data size to data+genPoly size
-  const paddedData = new Uint8Array(data.length + this.degree)
-  paddedData.set(data)
-
-  // The error correction codewords are the remainder after dividing the data codewords
-  // by a generator polynomial
-  const remainder = Polynomial.mod(paddedData, this.genPoly)
-
-  // return EC data blocks (last n byte, where n is the degree of genPoly)
-  // If coefficients number in remainder are less than genPoly degree,
-  // pad with 0s to the left to reach the needed number of coefficients
-  const start = this.degree - remainder.length
-  if (start > 0) {
-    const buff = new Uint8Array(this.degree)
-    buff.set(remainder, start)
-
-    return buff
+  /**
+   * Initialize the encoder.
+   * The input param should correspond to the number of error correction codewords.
+   */
+  initialize(degree: number) {
+    // create an irreducible generator polynomial
+    this.degree = degree
+    this.genPoly = Polynomial.generateECPolynomial(this.degree)
   }
 
-  return remainder
-}
+  /**
+   * Encodes a chunk of data
+   *
+   * @param  {Uint8Array} data Buffer containing input data
+   * @return {Uint8Array}      Buffer containing encoded data
+   */
+  encode(data: Uint8Array): Uint8Array {
+    if (!this.genPoly) {
+      throw new Error('Encoder not initialized')
+    }
 
-export default ReedSolomonEncoder
+    // Calculate EC for this data block
+    // extends data size to data+genPoly size
+    const paddedData = new Uint8Array(data.length + this.degree)
+    paddedData.set(data)
+
+    // The error correction codewords are the remainder after dividing the data codewords
+    // by a generator polynomial
+    const remainder = Polynomial.mod(paddedData, this.genPoly)
+
+    // return EC data blocks (last n byte, where n is the degree of genPoly)
+    // If coefficients number in remainder are less than genPoly degree,
+    // pad with 0s to the left to reach the needed number of coefficients
+    const start = this.degree - remainder.length
+    if (start > 0) {
+      const buff = new Uint8Array(this.degree)
+      buff.set(remainder, start)
+
+      return buff
+    }
+
+    return remainder
+  }
+}
