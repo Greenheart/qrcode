b856221e8f4e8de682e74894edf1c4d853827d32
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-03-13 20:16:11 +0100

    Formatting

--- test/unit/core/regex.test.ts
+++ test/unit/core/regex.test.ts
@@ -20,11 +20,16 @@ test('Regex test', () => {
   expect(Regex.TEST_NUMERIC.test('a12345'), 'Should return false if is not a number').toEqual(false)
   expect(Regex.TEST_NUMERIC.test('ABC123'), 'Should return false if is not a number').toEqual(false)
 
-  expect(Regex.TEST_ALPHANUMERIC.test('123ABC'), 'Should return true if is alphanumeric').toEqual(true)
-  expect(Regex.TEST_ALPHANUMERIC.test('123456'), 'Should return true if is alphanumeric').toEqual(true)
-  expect(Regex.TEST_ALPHANUMERIC.test('ABCabc'), 'Should return false if is not alphanumeric').toEqual(
-    false,
+  expect(Regex.TEST_ALPHANUMERIC.test('123ABC'), 'Should return true if is alphanumeric').toEqual(
+    true,
   )
+  expect(Regex.TEST_ALPHANUMERIC.test('123456'), 'Should return true if is alphanumeric').toEqual(
+    true,
+  )
+  expect(
+    Regex.TEST_ALPHANUMERIC.test('ABCabc'),
+    'Should return false if is not alphanumeric',
+  ).toEqual(false)
 
   expect(Regex.TEST_KANJI.test('乂ЁЖぞβ'), 'Should return true if is a kanji').toEqual(true)
   expect(Regex.TEST_KANJI.test('皿a晒三A'), 'Should return false if is not a kanji').toEqual(false)
